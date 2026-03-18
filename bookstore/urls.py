8d1ccd8c780aaa214954263c9a057235182b1b67
Rodrigo Silvestre <rodrigo291002@hotmail.com>
2026-03-18 13:40:23 -0300

    Updateeee

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from bookstore import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
+    re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
+    path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
+    path("update_server/", views.update, name="update"),
+    path('hello/', views.hello_world, name='hello_world'),
 ]
